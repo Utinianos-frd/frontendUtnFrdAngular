2ff4d68cfb56c73ef5ff1e34f75e38a8320f134b
alexlimavalle <alexlimavalle@gmail.com>
2026-05-17 13:24:32 -0300

    layout not found

--- src/app/features/not-found/not-found.ts
+++ src/app/features/not-found/not-found.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Location } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [RouterLink, ButtonModule],
   templateUrl: './not-found.html',
   styles: ``,
 })
-export class NotFound {}
+export class NotFound {
+  private location = inject(Location);
+
+  goBack() {
+    this.location.back();
+  }
+}
